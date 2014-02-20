042f85c357e743e6a0e410451039a30499563b45
luandefreitascarvalho <luandefreitascarvalho@gmail.com>
2014-02-20 14:46:55 +0000

    Aula 2

--- Odonto/src/odonto/modelo/entidade/Dentista.java
+++ Odonto/src/odonto/modelo/entidade/Dentista.java
@@ -8,5 +8,203 @@ package odonto.modelo.entidade;
  *
  */
 public class Dentista {
+	
+	private int id;
+	private String cro;
+	private String nome;
+	private String rg;
+	private String cpf;
+	
+	private Endereco endereco;
+	private Contato contato;
+	
+	public Dentista(int id, String cro, String nome, String rg, String cpf) {
+		this(cro, nome, rg, cpf);
+		this.id = id;
+	}
 
+	public Dentista(String cro, String nome, String rg, String cpf) {
+		this();
+		this.cro = cro;
+		this.nome = nome;
+		this.rg = rg;
+		this.cpf = cpf;
+	}
+	
+	public Dentista() {
+		super();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the cro
+	 */
+	public String getCro() {
+		return cro;
+	}
+
+	/**
+	 * @param cro the cro to set
+	 */
+	public void setCro(String cro) {
+		this.cro = cro;
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	/**
+	 * @return the rg
+	 */
+	public String getRg() {
+		return rg;
+	}
+
+	/**
+	 * @param rg the rg to set
+	 */
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
+
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	/**
+	 * @return the endereco
+	 */
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	/**
+	 * @param endereco the endereco to set
+	 */
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	/**
+	 * @return the contato
+	 */
+	public Contato getContato() {
+		return contato;
+	}
+
+	/**
+	 * @param contato the contato to set
+	 */
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((cro == null) ? 0 : cro.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Dentista other = (Dentista) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (cro == null) {
+			if (other.cro != null)
+				return false;
+		} else if (!cro.equals(other.cro))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
+		return true;
+	}
+
+	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Dentista [id= ")
+			.append(id)
+			.append(", cro= ")
+			.append(cro)
+			.append(", nome= ")
+			.append(nome)
+			.append(", rg= ")
+			.append(rg)
+			.append(", cpf= ")
+			.append(cpf)
+			.append(", endereco= ")
+			.append(endereco)
+			.append(", contato= ")
+			.append(contato)
+			.append(" ]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+	
 }
--- Odonto/src/odonto/modelo/entidade/Paciente.java
+++ Odonto/src/odonto/modelo/entidade/Paciente.java
@@ -14,7 +14,243 @@ public class Paciente {
 	private String rg;
 	private String cpf;
 	
+	private Contato contato;
+	
 	private SexoType sexo;
 	
 	private Endereco endereco;
+
+
+	public Paciente(long id, String nome, String rg, String cpf,
+			Contato contato, SexoType sexo, Endereco endereco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.contato = contato;
+		this.sexo = sexo;
+		this.endereco = endereco;
+	}
+
+
+	public Paciente(long id, String nome, String rg, String cpf, SexoType sexo) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+
+
+	public Paciente(String nome, String rg, String cpf, SexoType sexo) {
+		super();
+		this.nome = nome;
+		this.rg = rg;
+		this.cpf = cpf;
+		this.sexo = sexo;
+	}
+
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return nome;
+	}
+
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	/**
+	 * @return the rg
+	 */
+	public String getRg() {
+		return rg;
+	}
+
+
+	/**
+	 * @param rg the rg to set
+	 */
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return cpf;
+	}
+
+
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+
+	/**
+	 * @return the contato
+	 */
+	public Contato getContato() {
+		return contato;
+	}
+
+
+	/**
+	 * @param contato the contato to set
+	 */
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+
+
+	/**
+	 * @return the sexo
+	 */
+	public SexoType getSexo() {
+		return sexo;
+	}
+
+
+	/**
+	 * @param sexo the sexo to set
+	 */
+	public void setSexo(SexoType sexo) {
+		this.sexo = sexo;
+	}
+
+
+	/**
+	 * @return the endereco
+	 */
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+
+	/**
+	 * @param endereco the endereco to set
+	 */
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((contato == null) ? 0 : contato.hashCode());
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result
+				+ ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((rg == null) ? 0 : rg.hashCode());
+		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paciente other = (Paciente) obj;
+		if (contato == null) {
+			if (other.contato != null)
+				return false;
+		} else if (!contato.equals(other.contato))
+			return false;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (rg == null) {
+			if (other.rg != null)
+				return false;
+		} else if (!rg.equals(other.rg))
+			return false;
+		if (sexo != other.sexo)
+			return false;
+		return true;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Paciente [id= ")
+			.append(id)
+			.append(", nome= ")
+			.append(nome)
+			.append(", rg= ")
+			.append(rg)
+			.append(", cpf= ")
+			.append(cpf)
+			.append(", contato= ")
+			.append(contato)
+			.append(", sexo= ")
+			.append(sexo)
+			.append(", endereco= ")
+			.append(endereco)
+			.append(" ]");
+		return builder.toString();
+	}
+	
+	
+	
+	
 }
